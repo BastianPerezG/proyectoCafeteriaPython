516ecb08bb71fc014a52ad984fd7eceafa4c8e43
AryunDev <josematamoros916@gmail.com>
2022-11-07 14:08:04 -0300

    Agregue un nuevo parametro a clienteA redefiniendo el constructor en el archivo clientesPy

--- clientes.py
+++ clientes.py
@@ -2,8 +2,12 @@ from personas import Personas
 
 class Clientes(Personas):
     
-    pass
+    def __init__(self, run, nombre, app, puntos):
+        super().__init__(self, run, nombre, app)
+        self.puntos = puntos
+      
 
-clienteA = Clientes("run: 22.222.222-2", "Gabriel", "Manzano Valdivia")
+clienteA = Clientes("run: 22.222.222-2", "Gabriel", "Manzano Valdivia", "puntos acumulados: 3500")
 print(clienteA.ver_persona())
+print(clienteA.puntos)
         
