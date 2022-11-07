ec3b09304c013f38331e077e3bcc85d147832a02
AryunDev <josematamoros916@gmail.com>
2022-11-07 14:24:45 -0300

    comentarios sobre el codigo y algunas preguntas para resolver en la clase de hoy 07-11-2022

--- clientes.py
+++ clientes.py
@@ -1,13 +1,22 @@
+# decidimos separar las clases por archivo
+# por eso usamos este metodo para importar lo que esta en personas.py
 from personas import Personas
-
+# nuestra clase Clientes heredando () lo que venga de Personas
 class Clientes(Personas):
-    
+    # esta funcion agrega o redefine el constructor agregando
+    # "puntos" como nuevo parametro    
     def __init__(self, run, nombre, app, puntos):
+        # super().__init__(toma en consideracion estos parametros originales)
+        # del constructor en personas
         super().__init__(self, run, nombre, app)
+        # define el self para puntos en este caso quedo igual
         self.puntos = puntos
       
-
+# de que otras formas vamos agregando esta informacion? inputs? como definir cuando es entero y no string???
 clienteA = Clientes("run: 22.222.222-2", "Gabriel", "Manzano Valdivia", "puntos acumulados: 3500")
 print(clienteA.ver_persona())
+# recordar que este puntos que estamos llamando, es el que defini en el ultimo self.
 print(clienteA.puntos)
+
+# otras preguntas: para agregar parametros se hace 1 def para cada parametro adicional? como funciona eso?
         
--- personas.py
+++ personas.py
@@ -1,10 +1,14 @@
+#La class Personas contiene los elementos run, nombre, etc
 class Personas:
+    # constructor 
     def __init__(self, run, nombre, app):
 
         self.run = run
         self.nombre = nombre
         self.app = app
-
+    # metodo para leer las instancias persona
     def ver_persona(self):
+        # define lo que se pretende retornar
         txt="{0} {1}, {2}"
+        # retorna text en el formato run, nombre, app y lo que quiera agregar
         return txt.format(self.run, self.nombre, self.app)
