ba1325670b8e673a74466463cfb3e12bc1274d5e
AryunDev <josematamoros916@gmail.com>
2022-12-06 10:56:33 -0300

    a

--- conector.py
+++ conector.py
@@ -9,7 +9,7 @@ class DataBase:
             aux = connect(
                 host ='localhost',
                 user ='root',
-                password =  'Nokia2022', # getpass("Antes de continuar, debe que ingresar la clave de administrador de este sistema: \n-------->"),
+                password =  'abc.123', # getpass("Antes de continuar, debe que ingresar la clave de administrador de este sistema: \n-------->"),
                 database ='cafeteria'
             )
             self.connection = aux
